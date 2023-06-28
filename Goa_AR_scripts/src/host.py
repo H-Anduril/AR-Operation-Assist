ccdd165277a760cda2649dc81c4fb3bb4e93cea0
Hanran Wu <hanranwu0622@gmail.com>
2023-06-28 18:05:32 -0400

    Encapsulate stored procedures in py functions

--- Goa_AR_scripts/src/host.py
+++ Goa_AR_scripts/src/host.py
@@ -16,7 +16,20 @@ if __name__ == '__main__':
         exit(-1)
     request = "idle"
     while request != 'q':
-        request = input("press q to quit\n")
+        request = input("press q to quit; press 't' to test input; press 's' to test view\n")
+        if (request == 't'):
+            sql = """EXECUTE dbo.create_component ?, ?, ?, ?, ?"""
+            values = (-2, 'TEST', 'TEST_VENDOR', 'TEST_IMAGE', '/user/documents')
+            new_cursorPacket.cursor.execute(sql, values)
+            
+        elif request == 's':
+            new_cursorPacket.cursor.execute("SELECT * FROM dbo.component")
+            row = new_cursorPacket.cursor.fetchone()
+            while row:
+                print(row)
+                row = new_cursorPacket.cursor.fetchone()
+    
+        new_cursorPacket.connection.commit()
     
     new_cursorPacket.close()
     exit(0)
