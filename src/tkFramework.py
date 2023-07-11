6109ece1fda9a0df3ab9261b1f290c40198575ce
Hanran Wu <hanranwu0622@gmail.com>
2023-07-11 17:41:15 -0400

    add object config info for user

--- src/tkFramework.py
+++ src/tkFramework.py
@@ -37,6 +37,8 @@ class windows(tk.Tk):
         self.tagToID = {}
         self.tagToFileDir = {}
         
+        # self.idToShape
+        
         self.canvasSize = (0, 0)
         # we'll create the frames themselves later but let's add the components to the dictionary.
         for F in (DrawPage, CanvasConfigPage, CompletionScreen):
@@ -65,6 +67,9 @@ class DrawPage(tk.Frame):
         self.controller = controller
         self.scene = Canvas(self, width=self.controller.canvasSize[0], height=self.controller.canvasSize[1], bg="white", highlightthickness=1, highlightbackground="black")
         self.scene.pack()
+        
+        self.currImgConfigInfo = tk.Label(self, text="")
+        self.currImgConfigInfo.pack()
         
         self.scene.bind("<Button-1>", self.startMovement)
         self.scene.bind("<Button-3>", self.deleteImage)
@@ -110,6 +115,18 @@ class DrawPage(tk.Frame):
         load_file_button.pack()
         prev_button.pack()
         save_scene_button.pack(side="right", fill=tk.Y)
+    
+    def update_currInfo(self, event, reset=False):
+        if reset:
+            self.currImgConfigInfo.config(text="")
+            return
+        target = self.scene.find_closest(self.scene.canvasx(event.x), self.scene.canvasx(event.y), halo=5)
+        self.currImgConfigInfo.config(text=
+                              "Current Image Tag: {} | Current Image Center Coordinates (x, y): {} | Current Image Size: {}"
+                              .format(self.controller.idToTag[target[0]],
+                                        self.scene.coords(target[0]),
+                                        [(self.scene.bbox(target[0])[2] - self.scene.bbox(target[0])[0]), 
+                                        (self.scene.bbox(target[0])[3] - self.scene.bbox(target[0])[1])]))
         
     def load_file(self):
         tag = self.inputtext.get(1.0, "end")
@@ -140,7 +157,7 @@ class DrawPage(tk.Frame):
         # newImage = newImage.zoom(0.5, 0.5)
         self.controller.tagToPhotoImage[tag] = newPhotoImage
         self.controller.tagToImage[tag] = newImage
-        id = self.scene.create_image(250, 250, anchor=NW, image=newPhotoImage)
+        id = self.scene.create_image(250, 250, anchor=CENTER, image=newPhotoImage)
         self.controller.idToPhotoImage[id] = newPhotoImage
         self.controller.idToImage[id] = newImage
         self.controller.idToTag[id] = tag
@@ -155,7 +172,8 @@ class DrawPage(tk.Frame):
         print('startMovement init', self.initi_x, self.initi_y)
         self.movingimage = self.scene.find_closest(self.initi_x, self.initi_y, halo=5) # get canvas object ID of where mouse pointer is 
         print(self.movingimage)
-        print(self.scene.find_all()) # get all canvas objects ID 
+        print(self.scene.find_all()) # get all canvas objects ID
+        self.update_currInfo(event) 
 
     def stopMovement(self, event):
         self.move = False
@@ -176,24 +194,28 @@ class DrawPage(tk.Frame):
             # print(c.itemcget(self.movingimage, "filepath"))
             self.scene.move(self.movingimage, deltax, deltay) # move object
             self.scene.tag_raise(self.movingimage)
+            self.update_currInfo(event)
         
     def resize(self, event):
-        movingimg = self.scene.find_closest(self.cursorX, self.cursorY, halo=5)
-        x = self.scene.canvasx(event.x)
-        y = self.scene.canvasy(event.y)
-        print(movingimg)
-        id = movingimg[0]
-        img_w, img_h = self.controller.idToImage[id].size
-        if (event.delta == -120):
-            newImage = self.controller.idToImage[id].resize((img_w * 9 // 10 - 5, img_h * 9 // 10 - 5))
-            # self.scene.scale(movingimg, x, y, 0.9, 0.9)
-        elif (event.delta == 120):
-            newImage = self.controller.idToImage[id].resize((img_w * 11 // 10 + 5, img_h * 11 // 10 + 5))
-            # self.scene.scale(movingimg, x, y, 1.1, 1.1)
-        newPhotoImage = ImageTk.PhotoImage(newImage)
-        self.scene.itemconfig(id, image=newPhotoImage)
-        self.controller.idToImage[id] = newImage
-        self.controller.idToPhotoImage[id] = newPhotoImage
+        #if not self.objOutOfBound():
+        if True:
+            movingimg = self.scene.find_closest(self.cursorX, self.cursorY, halo=5)
+            x = self.scene.canvasx(event.x)
+            y = self.scene.canvasy(event.y)
+            print(movingimg)
+            id = movingimg[0]
+            img_w, img_h = self.controller.idToImage[id].size
+            if (event.delta == -120):
+                newImage = self.controller.idToImage[id].resize((img_w * 9 // 10 - 5, img_h * 9 // 10 - 5))
+                # self.scene.scale(movingimg, x, y, 0.9, 0.9)
+            elif (event.delta == 120):
+                newImage = self.controller.idToImage[id].resize((img_w * 11 // 10 + 5, img_h * 11 // 10 + 5))
+                # self.scene.scale(movingimg, x, y, 1.1, 1.1)
+            newPhotoImage = ImageTk.PhotoImage(newImage)
+            self.scene.itemconfig(id, image=newPhotoImage)
+            self.controller.idToImage[id] = newImage
+            self.controller.idToPhotoImage[id] = newPhotoImage
+            self.update_currInfo(event)
 
     def deleteImage(self, event):
         movingimg = self.scene.find_closest(self.cursorX, self.cursorY, halo=5)
@@ -208,6 +230,15 @@ class DrawPage(tk.Frame):
         del self.controller.tagToImage[tag]
         del self.controller.tagToID[tag]
         del self.controller.tagToFileDir[tag]
+        self.update_currInfo(event, reset=True)
+    
+    def objOutOfBound(self):
+        if (self.scene.bbox(self.movingimage)[0] < 0
+            or self.scene.bbox(self.movingimage)[1] < 0
+            or self.scene.bbox(self.movingimage)[2] > self.controller.canvasSize[0]
+            or self.scene.bbox(self.movingimage)[3] > self.controller.canvasSize[1]):
+            return True
+        return False
         
     def back_to_CanvasConfig(self):
         self.controller.idToImage = {}
@@ -319,7 +350,6 @@ class CanvasConfigPage(tk.Frame):
     def __str__(self):
         return "CanvasConfigPage"
 
-
 class CompletionScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -331,8 +361,7 @@ class CompletionScreen(tk.Frame):
         switch_window_button.pack(side="bottom", fill=tk.X)
         
     def __str__(self):
-        return "CompletionScreen"
-    
+        return "CompletionScreen"    
         
 if __name__ == "__main__":
     testObj = windows()
